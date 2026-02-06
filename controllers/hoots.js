800d877a6c22925551381281dc6434ed5d7cca0b
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2026-02-06 15:13:53 -0500

    Get hoot/hoots

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -1,10 +1,35 @@
 import Hoot from "../models/hoot.js";
 
-const getHoot = async (req, res) => {};
+const getHoots = async (req, res) => {
+  try {
+    const hoots = await Hoot.find({});
+    res.json(hoots);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const getHoots = async (req, res) => {};
+const getHoot = async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    res.json(hoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 const createHoot = async (req, res) => {
   try {
-  } catch (e) {}
+    req.body.author = req.user._id;
+    const hoot = await Hoot.create(req.body);
+    res.status(201).json(hoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export {
+  getHoots,
+  getHoot,
+  createHoot
 };
